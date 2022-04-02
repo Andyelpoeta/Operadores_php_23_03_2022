6640d567d44f8392d35d1601f2b03224f57d1346
andresjair <sotorodriguez4140@gmail.com>
2022-04-01 21:20:33 -0500

    first commit

---
+++ api.php
@@ -0,0 +1,26 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header("Content-type: application/json;charset=utf-8");
+$_DATA = (object) json_decode(file_get_contents("php://input"));
+$_DATA->SERVER = (string) $_SERVER["HTTP_HOST"];
+$_DATA->RES = (int) ($_DATA->OPERADOR == "and") 
+? (
+    ($_DATA->A and $_DATA->B) 
+        ? 1 
+        : 0
+    ) 
+: (
+    ($_DATA->OPERADOR == "or")  
+        ?(
+            ($_DATA->A || $_DATA->B) 
+            ? 1 
+            : 0
+        )
+        : ""
+);
+
+
+echo(json_encode($_DATA, JSON_PRETTY_PRINT));
+
+
+?>
---
+++ main.js
@@ -0,0 +1,60 @@
+let from = document.querySelector("#operadoresLogicos");
+let listaBotonos = document.querySelector("#btnDestino"); 
+let resAND = document.querySelector("#operadorAND");
+let resOR = document.querySelector("#operadorOR");
+let btnLimpiar = document.querySelector("#btnLimpiar");
+let mensajeServer = document.querySelector("#resServer");
+let nombre = from.action.split("/");
+for(let [id, input] of Object.entries(listaBotonos.children)){
+    input.value += ` ${nombre[2]+"/"+nombre[4]}`;
+}
+let input = (data)=>{
+    let json = {};
+    data.forEach(element => {
+        if(element.type != "submit"){
+            json[element.id] = Number(element.value);
+        }
+    });
+        return json;
+    }
+    let enviar = async(confi)=>{
+        let peticion = await fetch(...confi);
+        let json = await peticion.text();
+        json = JSON.parse(json);
+        console.log(json);
+        let plantilla = `
+        <tr>
+            <td>${json.A}</td>
+            <td>${json.B}</td>
+            <td>${json.RES}</td>
+        </tr>
+        `;
+        mensajeServer.innerText = json.SERVER;
+        switch (json.OPERADOR) {
+            case 'and':
+                resAND.insertAdjacentHTML("beforeend", plantilla);
+                break;
+            case 'or':
+                resOR.insertAdjacentHTML("beforeend", plantilla);
+                break;
+            default:
+    
+                break;
+        }
+    
+    
+    }
+    from.addEventListener("submit", (e)=>{
+        let json = input(Array.from(e.target));
+        json.OPERADOR = e.submitter.dataset.operador;
+        let confi = {
+            method: from.method, 
+        }
+        enviar([from.action,confi]);
+        e.preventDefault();
+    })
+    btnLimpiar.addEventListener("click", (e)=>{
+        mensajeServer.innerText = null;
+        resAND.innerText = null;
+        resOR.innerText = null;
+    })

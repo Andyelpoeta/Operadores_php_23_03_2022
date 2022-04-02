485ba34c076406faaf81639da87c2c256ebd94b2
andresjair <sotorodriguez4140@gmail.com>
2022-04-01 22:25:14 -0500

    Operadores quedaron bien

--- main.js
+++ main.js
@@ -1,28 +1,29 @@
-let from = document.querySelector("#operadoresLogicos");
-let listaBotonos = document.querySelector("#btnDestino"); 
-let resAND = document.querySelector("#operadorAND");
-let resOR = document.querySelector("#operadorOR");
-let btnLimpiar = document.querySelector("#btnLimpiar");
-let mensajeServer = document.querySelector("#resServer");
-let nombre = from.action.split("/");
-for(let [id, input] of Object.entries(listaBotonos.children)){
-    input.value += ` ${nombre[2]+"/"+nombre[4]}`;
-}
-
-let input = (data)=>{
-    let json = {};
-    data.forEach(element => {
-        if(element.type != "submit"){
-            json[element.id] = Number(element.value);
-        }
-    });
-        return json;
+addEventListener("DOMContentLoaded", (event)=>{
+    let from = document.querySelector("#operadoresLogicos");
+    let listaBotonos = document.querySelector("#btnDestino"); 
+    let resAND = document.querySelector("#operadorAND");
+    let resOR = document.querySelector("#operadorOR");
+    let btnLimpiar = document.querySelector("#btnLimpiar");
+    let mensajeServer = document.querySelector("#resServer");
+    let nombre = from.action.split("/");
+    for(let [id, input] of Object.entries(listaBotonos.children)){
+        input.value += ` ${nombre[2]+"/"+nombre[4]}`;
+    }
+    let input = (data)=>{
+        let json = {};
+        data.forEach(element => {
+            if(element.type != "submit"){
+                json[element.id] = Number(element.value);
+            }
+        });
+            return json;
     }
     let enviar = async(confi)=>{
         let peticion = await fetch(...confi);
         let json = await peticion.text();
+      
         json = JSON.parse(json);
-        console.log(json);
+       
         let plantilla = `
         <tr>
             <td>${json.A}</td>
@@ -48,8 +49,10 @@ let input = (data)=>{
     from.addEventListener("submit", (e)=>{
         let json = input(Array.from(e.target));
         json.OPERADOR = e.submitter.dataset.operador;
+        
         let confi = {
-            method: from.method, 
+            method: from.method,
+            body: JSON.stringify(json)
         }
         enviar([from.action,confi]);
         e.preventDefault();
@@ -59,3 +62,4 @@ let input = (data)=>{
         resAND.innerText = null;
         resOR.innerText = null;
     })
+})
